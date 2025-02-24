4852dd1ed9abc064cda3c7d685814b43629c1e57
Angelyr <scardking@gmail.com>
2025-02-24 14:39:08 -0500

    github action build

---
+++ install.sh
@@ -0,0 +1,11 @@
+git clone https://github.com/catchorg/Catch2 ../Catch2
+cmake -S ../Catch2 -B ../build-Catch2 \
+  -DCMAKE_INSTALL_PREFIX=../build-Catch2/install/
+cmake --build ../build-Catch2 -j8 --target install
+
+export CMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH:../build-Catch2/install
+
+cmake . -B ../build-class \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_CXX_COMPILER=g++
+cmake --build ../build-class -j8
---
+++ math.cpp
@@ -0,0 +1,10 @@
+#include <catch2/matchers/catch_matchers_floating_point.hpp>
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/catch_approx.hpp>
+
+TEST_CASE("catch2 math"){
+    REQUIRE(0.99999 == Catch::Approx(1));
+    REQUIRE_THAT(1.0, Catch::Matchers::WithinAbs(1.2, 0.2));
+    REQUIRE_THAT(0.0f, !Catch::Matchers::WithinAbs(1.0, 0.5));
+    REQUIRE_THAT(1.1, Catch::Matchers::WithinRel(1.0, 0.1));
+}
