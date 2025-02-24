89b9bc54d3854bd3b594301576cef6cc29cad0b4
Angelyr <scardking@gmail.com>
2025-02-24 14:51:15 -0500

    build with clang++

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+compiler=$1
+
 git clone https://github.com/catchorg/Catch2 ../Catch2
 cmake -S ../Catch2 -B ../build-Catch2 \
   -DCMAKE_INSTALL_PREFIX=../build-Catch2/install/
@@ -7,5 +9,5 @@ export CMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH:../build-Catch2/install
 
 cmake . -B ../build-class \
   -DCMAKE_BUILD_TYPE=Release \
-  -DCMAKE_CXX_COMPILER=g++
+  -DCMAKE_CXX_COMPILER=$compiler
 cmake --build ../build-class -j8
--- math.cpp
+++ math.cpp
@@ -6,5 +6,5 @@ TEST_CASE("catch2 math"){
     REQUIRE(0.99999 == Catch::Approx(1));
     REQUIRE_THAT(1.0, Catch::Matchers::WithinAbs(1.2, 0.2));
     REQUIRE_THAT(0.0f, !Catch::Matchers::WithinAbs(1.0, 0.5));
-    REQUIRE_THAT(1.1, Catch::Matchers::WithinRel(1.0, 0.1))
+    REQUIRE_THAT(1.1, Catch::Matchers::WithinRel(1.0, 0.1));
 }
